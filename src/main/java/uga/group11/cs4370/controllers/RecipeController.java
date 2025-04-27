e24eef62f2185a96e4194e898b85ddbdd655db9d
= <lukejmullins@gmail.com>
2025-04-27 14:08:10 -0400

    Basic Framework

---
+++ src/main/java/uga/group11/cs4370/controllers/RecipeController.java
@@ -0,0 +1,23 @@
+package uga.group11.cs4370.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/recipe")
+public class RecipeController {
+@GetMapping
+public ModelAndView webpage(@RequestParam(name = "error", required = false) String error) {
+ ModelAndView mv = new ModelAndView("recipe_page");
+
+      
+    return mv;
+    
+}
+}
---
+++ src/main/java/uga/group11/cs4370/models/User.java
@@ -0,0 +1,5 @@
+package uga.group11.cs4370.models;
+
+public class User {
+    
+}
