dffce0b9c6935898cf128c36f6225c7fe6374793
Lan18 <landonmc02@gmail.com>
2025-04-28 14:59:15 -0400

    added a basic profile card to homepage with docs on how to dynamically integrate

--- src/main/java/uga/group11/cs4370/App.java
+++ src/main/java/uga/group11/cs4370/App.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * This is the application entry point. Do not change this unless you know 
+ * This is the application entry point. Do not change this unless you know
  * what you are doing.
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/uga/group11/cs4370/controllers/ChefsController.java
+++ src/main/java/uga/group11/cs4370/controllers/ChefsController.java
@@ -4,20 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
-
 
 @Controller
 @RequestMapping("/chefs")
 public class ChefsController {
-@GetMapping
-public ModelAndView webpage(@RequestParam(name = "error", required = false) String error) {
- ModelAndView mv = new ModelAndView("chefs_page");
+    @GetMapping
+    public ModelAndView webpage(@RequestParam(name = "error", required = false) String error) {
+        ModelAndView mv = new ModelAndView("chefs_page");
 
-      
-    return mv;
-    
-}
+        return mv;
+
+    }
 }
